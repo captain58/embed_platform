fab8d4f2e66686ff5ba6428054598c1ea5b6782d
captain58 <general123@163.com>
2021-06-28 08:06:29 +0800

    文件

---
+++ platform/mcu/GD32F30x/chip.h
@@ -0,0 +1,41 @@
+#ifndef __CHIP_H_
+#define __CHIP_H_
+
+//	#include "FM33G0XX.h"
+//	/* ================================================================================ */
+//	/* ================             Peripheral include                 ================ */
+//	/* ================================================================================ */
+//	
+//	//#include "fm33A0xx_aes.h"
+//	#include "fm33G0xx_anac.h"
+//	#include "fm33G0xx_btim.h"
+//	//#include "fm33A0xx_crc.h"
+//	//#include "fm33A0xx_dma.h"
+//	#include "fm33G0xx_etim.h"
+//	#include "fm33G0xx_flash.h"
+//	#include "fm33G0xx_gpio.h"
+//	#include "fm33G0xx_i2c.h"
+//	#include "fm33G0xx_iwdt.h"
+//	#include "fm33G0xx_lcd.h"
+//	#include "fm33G0xx_lptim.h"
+//	#include "fm33G0xx_rcc.h"
+//	#include "fm33G0xx_rtc.h"
+//	#include "fm33G0xx_scu.h"
+//	#include "fm33G0xx_spi.h"
+//	#include "fm33G0xx_hspi.h"
+//	//#include "fm33A0xx_trng.h"
+//	//#include "fm33A0xx_u7816.h"
+//	#include "fm33G0xx_uart.h"
+//	#include "fm33G0xx_wwdt.h"
+//	#include "fm33G0xx_pmu.h"
+
+
+//	#include "stm32f030x8.h"
+//	#include "stm32f0xx_hal_conf.h"
+//#include "stm32f0xx_hal_def.h"
+#include "gd32f30x_usart.h"
+#include "gd32f30x_gpio.h"
+//#include "stm32f0xx_hal_def.h"
+//#include "sysext.h"
+#endif
+
